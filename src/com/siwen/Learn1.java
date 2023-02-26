7e1de74a8171077f05857546f4e71273432ba7b1
XiaoWen Su <2014863935@qq.com>
2023-02-26 16:09:43 +0800

    firstly use eclipse

---
+++ src/com/siwen/Learn1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.siwen;
+
+/**
+ * @author 20148
+ *
+ */
+public class Learn1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+	}
+}
+
